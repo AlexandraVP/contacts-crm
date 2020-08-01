60e5c4b7931545f260f0f728ff69d5edb4b276f1
Alexandra Pryakhina <eissa.2310@gmail.com>
2020-08-01 16:37:19 +0300

    error handling

--- src/app.js
+++ src/app.js
@@ -11,23 +11,24 @@ const dataUrls = {
     large: 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
 };
 
-function loadData(name){
-    return new Promise(resolve => {
-        if(dataCache[name]){
+function loadData(name) {
+    return new Promise((resolve, reject) => {
+        if (dataCache[name]) {
             resolve(dataCache[name]);
-        }else{
+        } else {
             fetch(dataUrls[name])
                 .then(d => d.json())
                 .then((contacts) => {
-                    dataCache[name] = contacts.filter((c,i) => contacts.findIndex(d => d.id === c.id) === i);
+                    dataCache[name] = contacts.filter((c, i) => contacts.findIndex(d => d.id === c.id) === i);
                     resolve(dataCache[name]);
-                });
+                })
+                .catch(reject);
         }
     })
 }
 
-function getMaxId(contacts){
-    return contacts.reduce((maxId, contact) => maxId > contact.id ? maxId : contact.id , 0)
+function getMaxId(contacts) {
+    return contacts.reduce((maxId, contact) => maxId > contact.id ? maxId : contact.id, 0)
 }
 
 export class App extends Component {
@@ -55,14 +56,18 @@ export class App extends Component {
     };
 
     load = (page) => {
-        if(this.state.processing){
-             return;
+        if (this.state.processing) {
+            return;
         }
-        loadData(page).then(contacts => {
-            const nextId = getMaxId(contacts) + 1;
-            this.setState({contacts, nextId, processing: false});
-        });
-        this.setState({page, processing: true});
+        loadData(page)
+            .then(contacts => {
+                const nextId = getMaxId(contacts) + 1;
+                this.setState({contacts, nextId, processing: false});
+            })
+            .catch(e => {
+                this.setState({processing: false, error: true});
+            });
+        this.setState({page, processing: true, error: false});
     };
 
     render() {
@@ -72,20 +77,23 @@ export class App extends Component {
                     <ul className="nav nav-tabs card-header-tabs">
                         <li className="nav-item">
                             <div className={this.state.page === 'small' ? 'nav-link active' : 'nav-link'}
-                                    onClick={()=>this.load('small')}>
+                                 onClick={() => this.load('small')}>
                                 Маленький объем
                             </div>
                         </li>
                         <li className="nav-item">
                             <div className={this.state.page === 'large' ? 'nav-link active' : 'nav-link'}
-                                 onClick={()=>this.load('large')}>
+                                 onClick={() => this.load('large')}>
                                 Большой объем
                             </div>
                         </li>
                     </ul>
                 </div>
                 <div className="card-body">
-                    {!this.state.page && "Выберите набор данных для отображения таблицы."}
+                    {!this.state.page && (
+                        <div className="alert alert-primary" role="alert">
+                            Выберите набор данных для отображения таблицы.
+                        </div>)}
                     {this.state.processing && (
                         <div className="d-flex justify-content-center">
                             <div className="spinner-border" role="status">
@@ -93,8 +101,13 @@ export class App extends Component {
                             </div>
                         </div>
                     )}
-                    {this.state.page && !this.state.processing &&
-                        <TableWithControls contacts={this.state.contacts} addContact={this.addContact}/>
+                    {this.state.error && (
+                        <div className="alert alert-danger" role="alert">
+                            Произошла ошибка загрузки данных. Возможно сервис недоступен.
+                            Попробуйте еще раз выбрать набор данных позже.
+                        </div>)}
+                    {!this.state.error && this.state.page && !this.state.processing &&
+                    <TableWithControls contacts={this.state.contacts} addContact={this.addContact}/>
                     }
                 </div>
             </div>
